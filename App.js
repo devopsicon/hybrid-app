ab2d7ad6ca831c8b20c6c073cc07c55345045a91
angelinb <angelinb@gmail.com>
2018-04-16 22:08:31 -0400

    Sales Dashboard Sample

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Alert, AppRegistry, Button, StyleSheet, View, Text } from 'react-native';
+import { Alert, AppRegistry, Button, StyleSheet, View, Text ,FlatList, ActivityIndicator} from 'react-native';
 import {
   StackNavigator,
 } from 'react-navigation';
-
+import { Col, Row, Grid } from "react-native-easy-grid";
 
 class HomeScreen extends React.Component {
   render() {
@@ -21,16 +21,65 @@ class HomeScreen extends React.Component {
     );
   }
 }
+  
 
-class SalesScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Sales Screen</Text>
+
+
+class SalesScreen extends React.Component  {
+
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true}
+  }
+
+  componentDidMount(){
+    return fetch('https://raw.githubusercontent.com/yuya373/spark-tutorial/master/src/main/resources/sales.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson,
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+
+
+  render(){
+
+
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+    return(
+
+    
+
+      <View style={{flex: 1, paddingTop:20}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.customerId}, {item.amountPaid},{item.transactionId}</Text>}
+          keyExtractor={(item, index) => index}
+        />
       </View>
     );
   }
 }
+
+
+
 
 class ExpensesScreen extends React.Component {
   render() {
